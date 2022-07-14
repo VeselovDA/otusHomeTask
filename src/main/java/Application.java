61c7795beb8965f378477f3c90922e67a8c7f13a
Веселов Д.А <veselov.da@reliab.tech>
2022-07-14 10:39:32 +0300

    init spring app with xml context

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("/spring-context.xml");
+    }
+}
