384f427cae03b552d562a2eb3ea699f0d1f5a60c
veselov.da <veselov.da@reliab.tech>
2022-07-14 22:30:38 +0300

    add application.properties

--- src/main/java/service/impl/CsvReaderServiceImpl.java
+++ src/main/java/service/impl/CsvReaderServiceImpl.java
@@ -1,14 +1,20 @@
 package service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
 import service.ReaderService;
 import util.CsvReader;
 
 import java.util.List;
-
+@Service("readerService")
+@PropertySource("classpath:application.properties")
 public class CsvReaderServiceImpl implements ReaderService {
+    @Value("${resourcePath}")
+    private String resourcePath;
 
     @Override
     public List<String[]> readAllQuestions() {
-        return CsvReader.readResource();
+        return CsvReader.readResource(resourcePath);
     }
 }
