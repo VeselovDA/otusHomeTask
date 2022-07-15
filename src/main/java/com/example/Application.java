ed07abf3e929f0e34f5c5facdda167989f256e92
Веселов Д.А <veselov.da@reliab.tech>
2022-07-15 15:34:38 +0300

    add Spring Boot

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,21 +1,13 @@
 package com.example;
 
-import com.example.service.PrintService;
-import com.example.service.QuestionService;
 import com.example.service.TestingService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-import com.example.persistence.model.Question;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-import java.util.List;
-
-@ComponentScan
-@Component
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(Application.class);
+        var context =  SpringApplication.run(Application.class);
 
         var testingService = (TestingService) context.getBean("testingService");
 
