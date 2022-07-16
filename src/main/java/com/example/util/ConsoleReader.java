5ca92781f52384993906d96e46edfb83175bbd0b
veselov.da <veselov.da@reliab.tech>
2022-07-16 13:23:18 +0300

    ref: transfer message to service

--- src/main/java/com/example/util/ConsoleReader.java
+++ src/main/java/com/example/util/ConsoleReader.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ConsoleReader {
     private static Scanner in = new Scanner(System.in);
     public static String readConsole(){
-        System.out.print("Write an answer: ");
         String str = in.nextLine();
        return str;
     }
